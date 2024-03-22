3f64a56e0b88fa21e27deaddb59fdc55e1bcc03c
Daniel Guerrero <danxguerrero@google.com>
2024-03-22 13:01:03 -0400

    Rendered the search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { Container, Form, Navbar } from 'react-bootstrap';
+import './App.css';
+
+const pokeApi = 'https://pokeapi.co/api/v2/pokemon?limit=151'
 
 function App() {
+  const [allPokemon, setAllPokemon] = useState([]);
+
+  useEffect(()=>{
+    fetch(pokeApi)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllPokemon(data);
+      })
+  }, [])
   
 
   return (
     <>
+      <Navbar bg="dark" data-bs-theme="dark" expand="lg" fixed="top">
+        <Container style={{display: 'flex', justifyContent: 'start', marginLeft: '1em'}}>
+          <Navbar.Brand>Pokeverse</Navbar.Brand>
+        </Container>
+      </Navbar>
 
+
+      <Form>
+        <Form.Group>
+          <Form.Label>Search</Form.Label>
+          <Form.Control placeholder='Search..'/>
+        </Form.Group>
+      </Form>
     </>
   )
 }
