5965164f0cb4aa49af2bb10f4a303f2cb286e653
danxguerrero <danxguerrero@yahoo.com>
2024-03-23 23:17:45 -0400

    Add yellow border  and box shadow around image card.

--- components/Pokemon.jsx
+++ components/Pokemon.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Card } from 'react-bootstrap'
 
-export const Pokemon = ({allPokemon, pokemon}) => {
+export const Pokemon = ({ allPokemon, pokemon }) => {
     const [pokemonData, setPokemonData] = useState({})
 
     async function fetchPokemonData() {
@@ -9,8 +9,8 @@ export const Pokemon = ({allPokemon, pokemon}) => {
             const res = await fetch(pokemon.url);
             const data = await res.json();
             setPokemonData(data);
-        } catch(error) {
-        console.log("There was an error fetching Pokemon data")
+        } catch (error) {
+            console.log("There was an error fetching Pokemon data")
         }
     }
 
@@ -20,27 +20,26 @@ export const Pokemon = ({allPokemon, pokemon}) => {
 
     return (
         (!pokemonData ? "Loading" :
-            (<Card 
+            (<Card
                 className={pokemonData.types[0].type.name}
                 style={{
-                height: '300px',
-                width: '200px',
-                margin: '10px',
-                border: 'solid 5px yellow',
-                padding: '15px',
-            
-        }}> 
-            <div style={{display:'flex', alignItems:'start'}}>
-                <Card.Title style={{fontSize: '16px', textTransform: 'capitalize'}}>{pokemonData.name}</Card.Title>
-            </div>
-            { pokemonData.sprites && (<Card style={{height:'100px', width: '90%', margin: 'auto', display: 'flex', alignItems: 'center', paddingBottom: '130px', backgroundImage:'linear-gradient(to bottom, white, silver,)'}}>
-                <Card.Img alt={pokemonData.name} src={pokemonData.sprites.front_default}></Card.Img>
-            </Card>)}
-            <Card.Body>
-                {pokemonData.moves && <p>{pokemonData.moves[0].move.name}</p>}
-            </Card.Body> 
-        </Card>))
+                    height: '300px',
+                    width: '200px',
+                    margin: '10px',
+                    border: 'solid 8px yellow',
+                    padding: '15px',
+
+                }}>
+                <div style={{ display: 'flex', alignItems: 'start' }}>
+                    <Card.Title style={{ fontSize: '16px', textTransform: 'capitalize' }}>{pokemonData.name}</Card.Title>
+                </div>
+                {pokemonData.sprites && (
+                    <Card style={{ height: '100px', width: '90%', margin: 'auto', display: 'flex', alignItems: 'center', paddingBottom: '130px', border: 'solid 2.5px yellow', boxShadow: '5px 5px 8px #888888', backgroundImage: 'linear-gradient(to bottom, white, silver,)' }}>
+                        <Card.Img alt={pokemonData.name} src={pokemonData.sprites.front_default}></Card.Img>
+                    </Card>)}
+                <Card.Body>
+                    {pokemonData.moves && <p>{pokemonData.moves[0].move.name}</p>}
+                </Card.Body>
+            </Card>))
     )
 }
-
-// pokemonData.sprites.front_default
