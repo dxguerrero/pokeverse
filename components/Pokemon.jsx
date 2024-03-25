9e8706370d5f3310e6cb52cac2ce548dd8826224
danxguerrero <danxguerrero@yahoo.com>
2024-03-24 23:25:03 -0400

    Added pokemon card blanks as the card background image

--- components/Pokemon.jsx
+++ components/Pokemon.jsx
@@ -23,18 +23,17 @@ export const Pokemon = ({ allPokemon, pokemon }) => {
             (<Card
                 className={pokemonData.types[0].type.name}
                 style={{
-                    height: '300px',
-                    width: '200px',
+                    height: '425px',
+                    width: '303px',
                     margin: '10px',
-                    border: 'solid 8px yellow',
                     padding: '15px',
-
+                    borderRadius: '20px'
                 }}>
                 <div style={{ display: 'flex', alignItems: 'start' }}>
                     <Card.Title style={{ fontSize: '16px', textTransform: 'capitalize' }}>{pokemonData.name}</Card.Title>
                 </div>
                 {pokemonData.sprites && (
-                    <Card style={{ height: '100px', width: '90%', margin: 'auto', display: 'flex', alignItems: 'center', paddingBottom: '130px', border: 'solid 2.5px yellow', boxShadow: '5px 5px 8px #888888', backgroundImage: 'linear-gradient(to bottom, white, silver,)' }}>
+                    <Card style={{ height: '100px', width: '90%', margin: 'auto', display: 'flex', alignItems: 'center', paddingBottom: '130px', border: 'solid 2.5px yellow', boxShadow: '5px 5px 8px #888888', backgroundImage: 'red' }}>
                         <Card.Img alt={pokemonData.name} src={pokemonData.sprites.front_default}></Card.Img>
                     </Card>)}
                 <Card.Body>
