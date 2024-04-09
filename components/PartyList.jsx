6f0e36e56b706414b0a2ad341d69e757db789c0c
danxguerrero <danxguerrero@yahoo.com>
2024-04-08 22:49:47 -0400

    Added more styling structure to Party List Cards

--- components/PartyList.jsx
+++ components/PartyList.jsx
@@ -12,7 +12,7 @@ export const PartyList  = () => {
 
     return (
         <Card style={{height: '100%', width: '100%', background: 'none', border: "none", backgroundColor: 'none', overflow: 'hidden'}}>
-                <Card style={{borderRadius: '90px', backgroundColor: 'rgb(14, 122, 97)', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                <Card style={{borderRadius: '90px', backgroundColor: 'rgb(14, 122, 97)', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
                     <Col style={{width: '100%'}}>
                         <Row style={{marginBottom: '10px', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%'}}>
                             <Card.Title className='white-text' style={{width: '100%'}}>Current Party</Card.Title>
@@ -22,20 +22,22 @@ export const PartyList  = () => {
                 <Col style={{marginTop: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'start'}}>
                     {party &&
                         party.map((pokemon, index) => (
-                            <Card key={index} style={{backgroundColor: 'rgb(14, 122, 97)', margin: '5px', height: '25%', width: '100%'}}>
+                            <Card key={index} style={{backgroundColor: 'rgb(14, 122, 97)', margin: '5px', height: '25%', width: '100%', padding: '4px 4px'}}>
                                 <Container>
                                     <Row>
-                                        <Col>
-                                            <Row style={{display: 'flex', flexDirection: 'row'}}>
-                                                <h1 className='white-text' style={{fontSize:'12px'}}>Lv. 100</h1>
-                                                <Button variant='danger' style={{borderRadius: '100%', width: '30%', height: '30%'}} onClick={() => removeFromParty(index)}>X</Button>
+                                        <Col style={{width: '75%'}}>
+                                            <Row style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+                                                <div style={{display: 'inline-block', alignItems: 'center', justifyContent: 'center', backgroundColor: 'rgb(10, 76, 61)', width: '60%', height: '50%', borderRadius: '50px'}}>
+                                                    <h1 className='white-text' style={{fontSize:'22px'}}>Lv. 100</h1>
+                                                </div>
+                                                <Button variant='danger' style={{borderRadius: '100px', width: '30px', height: '30px'}} onClick={() => removeFromParty(index)}>X</Button>
                                             </Row>
                                             <Row>
                                             <Card.Title className='white-text'>{pokemon.name}</Card.Title>
                                             </Row>
                                         </Col>
-                                        <Col style={{display: 'flex', justifyContent: 'end', flexDirection: 'row', width: '100%'}}>
-                                                <img src={pokemon.sprites.other.showdown.front_default} style={{height: '75%', width: '75%'}}/>
+                                        <Col style={{display: 'flex', justifyContent: 'end', flexDirection: 'row', width: '50%'}}>
+                                                <img src={pokemon.sprites.other.showdown.front_default} style={{height: `${pokemon.height > 10 ? '100%' : '75%'}`, width: `${pokemon.height > 10 ? '50%' : '37.5%'}`}}/>
                                         </Col>
                                     </Row>
                                 </Container>
